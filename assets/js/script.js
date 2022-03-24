7d5ede629f103104523c4fad3bbc1121962d0860
Carol <carol.tsai0224@gmail.com>
2022-03-24 15:18:02 -0400

    color coded time blocks based on past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
-var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+var timeblocksEl = $('#timeblocks');
+
+var rightnow = moment();
+$("#currentDay").text(rightnow.format("MMM Do, YYYY"));
+
+var time = rightnow.format('HH')
+console.log(hour);
+
+var timeblocks = timeblocksEl.children();
+console.log(timeblocks);
+
+for(i=0; i<timeblocks.length; i++) {
+   var hour = timeblocks.eq(i);
+   var hourData = hour.attr('data-hour');
+   var hourText = hour.children().eq(1);
+   console.log(hour);
+   if (hourData < time) {
+      hourText.addClass('bg-secondary');
+   } else if (hourData == time) {
+      hourText.addClass('bg-success');
+   } else {
+      hourText.addClass('bg-info');
+   }
+}
