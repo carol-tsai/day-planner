fcc9b032a7ce56ffd5360bf5eddd68ccf1a14ab0
Carol <carol.tsai0224@gmail.com>
2022-03-24 17:25:23 -0400

    save text input to local storage

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # day-planner
+
+### Resources
+https://api.jquery.com/click/
+https://getbootstrap.com/docs/4.0/layout/grid/
+https://getbootstrap.com/docs/4.0/utilities/colors/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,55 @@
+// Global Variables
 var timeblocksEl = $('#timeblocks');
-
 var rightnow = moment();
+var time = rightnow.format('HH');
+var timeblocks = timeblocksEl.children();
+
+// Functions
+
+// Save input text to local storage
+function saveText(event) {
+   event.preventDefault();
+   var saveBtn = $(event.target);
+   var text = saveBtn.parent().children('input').val();
+   var time = saveBtn.parent().children('label').attr('for');
+   console.log(time);
+   localStorage.setItem(time, text);
+   return;
+}
+
+// Fetch text from local storage
+function fetchText(key, box) {
+   if (localStorage.getItem(key) != null) {
+      var text = localStorage.getItem(key);
+      box.val(text);
+   }
+}
+// Color code hour boxes based on time of day
+function colorCode(data, box) {
+   if (data < time) {
+      box.addClass('past');
+   } else if (data == time) {
+      box.addClass('present');
+   } else {
+      box.addClass('future');
+   }
+}
+
+// Set Date
 $("#currentDay").text(rightnow.format("MMM Do, YYYY"));
 
-var time = rightnow.format('HH')
-console.log(hour);
-
-var timeblocks = timeblocksEl.children();
-console.log(timeblocks);
-
+// Set up planner
 for(i=0; i<timeblocks.length; i++) {
    var hour = timeblocks.eq(i);
    var hourData = hour.attr('data-hour');
+   var hourLabel = hour.children('label').attr('for');
    var hourText = hour.children().eq(1);
-   console.log(hour);
-   if (hourData < time) {
-      hourText.addClass('bg-secondary');
-   } else if (hourData == time) {
-      hourText.addClass('bg-success');
-   } else {
-      hourText.addClass('bg-info');
-   }
+   var hourSave = hour.children().eq(2);
+   fetchText(hourLabel, hourText);
+   colorCode(hourData, hourText);
+   hourSave.click(saveText);
 }
+
+
+
+
